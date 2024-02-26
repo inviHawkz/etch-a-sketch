73ca3ca045b1e30f7dc9eeffd2a52b08bbc6fd4c
inviHawkz <inviHawkz@gmail.com>
2024-02-26 22:17:26 +0700

    Add some html, no new function

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const container = document.querySelector('#container');
+const canvas = document.querySelector('#canvas');
 
-for ( let i = 0; i < 1024; i++) {
-    const pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    container.appendChild(pixel);
+for ( let i = 0; i < 1369; i++) {
+    const square = document.createElement('div');
+    square.className = 'square';
+    canvas.appendChild(square);
 }
 
-const pixelList = document.querySelectorAll('.pixel');
-pixelList.forEach(pixel => pixel.addEventListener('mouseover', e => { if (e.ctrlKey) {e.target.classList.add('transform');
+const pixelList = document.querySelectorAll('.square');
+pixelList.forEach(square => square.addEventListener('mouseover', e => { if (e.ctrlKey) {e.target.classList.add('transform');
                                                                     } else if (e.altKey) {e.target.classList.remove('transform');}}));
-document.querySelector('button').addEventListener('click', () => pixelList.forEach(pixel => pixel.classList.remove('transform')));
+document.querySelector('button').addEventListener('click', () => pixelList.forEach(square => square.classList.remove('transform')));
+
+let canvasHeight = 666;
+let canvasWidth = 666;
+
+//canvas height: 666px
+//canvas width: 666px
+//square pixel: 18x18 (16x16 + 1px border each side)
+//square per side: 37 (666/18)
+//total square: 1369 (37*37)
